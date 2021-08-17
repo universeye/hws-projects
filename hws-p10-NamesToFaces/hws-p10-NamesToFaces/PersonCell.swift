005af59a1de12a3fd3c403ff9eab317acd5ae43d
Terry <monthrnb@gmail.com>
2021-08-17 23:50:24 +0800

    add project 10

---
+++ hws-p10-NamesToFaces/hws-p10-NamesToFaces/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  hws-p10-NamesToFaces
+//
+//  Created by Terry Kuo on 2021/8/17.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
---
+++ hws-p10-NamesToFaces/hws-p10-NamesToFaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hws-p10-NamesToFaces
+//
+//  Created by Terry Kuo on 2021/8/17.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
