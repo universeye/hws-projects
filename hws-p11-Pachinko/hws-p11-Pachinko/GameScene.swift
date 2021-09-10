8bdaed5a02239db75ce446c49717fad856ce86f0
Terry <monthrnb@gmail.com>
2021-09-11 01:18:43 +0800

    start hws p11

---
+++ hws-p11-Pachinko/hws-p11-Pachinko/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  hws-p11-Pachinko
+//
+//  Created by Terry Kuo on 2021/9/11.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
---
+++ hws-p11-Pachinko/hws-p11-PachinkoTests/hws_p11_PachinkoTests.swift
@@ -0,0 +1,33 @@
+//
+//  hws_p11_PachinkoTests.swift
+//  hws-p11-PachinkoTests
+//
+//  Created by Terry Kuo on 2021/9/11.
+//
+
+import XCTest
+@testable import hws_p11_Pachinko
+
+class hws_p11_PachinkoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
