8e85554d39100b0ebe67ab985ad61ad045830137
Terry <terrykuo501@gmail.com>
2021-09-13 12:28:26 +0800

    Initialize

--- hws-p11-Pachinko/hws-p11-Pachinko/GameScene.swift
+++ hws-p11-Pachinko/hws-p11-Pachinko/GameScene.swift
@@ -11,10 +11,19 @@ import GameplayKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384) //center of landscape ipad
+        background.blendMode = .replace
+        background.zPosition = -1 //put it behind everything
+        addChild(background)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
+            box.position = location
+            addChild(box)
+        }
     }
 }
